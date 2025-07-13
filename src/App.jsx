5e48a16061bfebd58fc4fb18bdb1515ddd666b94
Shreeyog-Gaikwad <shreeyoggaikwad@gmail.com>
2025-07-13 14:04:06 +0530

    Vite, Tailwind and Necessary Dependencies Added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import appwrite from './config/config'
+
+function App() {
+
+  return (
+    <>
+      <div className=' text-8xl text-center'>EchoVerse - A Blog Website</div>
+    </>
+  )
+}
+
+export default App
